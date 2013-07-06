950ff18ee499fb39f295b0cf15009e1ceebf09a2
Joseph Lord <joseph@human-friendly.com>
2013-07-06 22:37:42 +0100

    Corrected centering to horizontal.

--- AutolayoutScrollviewDemonstration/HFViewController.m
+++ AutolayoutScrollviewDemonstration/HFViewController.m
@@ -26,4 +26,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewWillLayoutSubviews
+{
+    //    CGSize topsize = self.view.frame.size;
+    //    UIScrollView * scrlView = (UIScrollView *)self.view.subviews[0]; // Should use outlet but it does the job.
+    //    scrlView.contentSize = CGSizeMake(self.topsize.width, scrlView.contentSize.height);
+}
 @end
